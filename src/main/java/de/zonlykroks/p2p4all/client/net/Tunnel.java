87ae4b8391989147372a991ff433f014e26714af
four-griffins <four.griffins.dev@gmail.com>
2024-01-14 15:45:28 +0100

    store tunnel so garbage collector doesnt eat it

--- src/main/java/de/zonlykroks/p2p4all/client/net/Tunnel.java
+++ src/main/java/de/zonlykroks/p2p4all/client/net/Tunnel.java
@@ -11,6 +11,7 @@ public class Tunnel {
     private final int sourcePort;
     private final InetSocketAddress target;
     private final boolean isServer;
+    private Socket remote;
 
     public Tunnel(int sourcePort, String dest, boolean isServer) {
         this.sourcePort = sourcePort;
@@ -24,24 +25,23 @@ public class Tunnel {
     public void start() throws SocketException {
         System.out.printf("Starting hole punching: [:%d] --- [%s:%d]\n",
                 sourcePort, target.getAddress(), target.getPort());
-
-        Socket remote = new Socket();
+        
         InetSocketAddress localAddr = new InetSocketAddress(sourcePort);
 
         for (int i = 0; i < 20; i++) {
             System.out.printf("punch attempt %d / %d\n", i+1, 20);
             try {
-                remote = new Socket();
-                remote.setReuseAddress(true);
-                remote.bind(localAddr);
-                remote.connect(target, isServer ? 2200 : 1800);
+                this.remote = new Socket();
+                this.remote.setReuseAddress(true);
+                this.remote.bind(localAddr);
+                this.remote.connect(target, isServer ? 2200 : 1800);
                 System.out.println("connection established as client!");
                 break;
             } catch (IOException e) {
                 System.out.println("IOEx punch clientside bind: " + e.getMessage());
                 try {
-                    remote.close();
-                    remote = null;
+                    this.remote.close();
+                    this.remote = null;
                 } catch (IOException ex) {
                     System.out.println("IOEx punch clientside close: " + e.getMessage());
                 }
@@ -51,34 +51,37 @@ public class Tunnel {
                 s.setReuseAddress(true);
                 s.setSoTimeout(isServer ? 3000 : 2500);
                 s.bind(localAddr);
-                remote = s.accept();
+                this.remote = s.accept();
                 System.out.println("connection established as server!");
                 break;
             } catch (IOException e) {
                 System.out.println("IOEx punch serverside bind: " + e.getMessage());
                 try {
-                    if (remote != null) remote.close();
-                    remote = null;
+                    if (this.remote != null) this.remote.close();
+                    this.remote = null;
                 } catch (IOException ex) {
                     System.out.println("IOEx punch serverside close: " + ex.getMessage());
                 }
             }
         }
 
-        if (remote == null) {
+        if (this.remote == null) {
             System.out.println("failed to setup tunnel, aborting");
             throw new SocketException("failed to setup tunnel");
         }
 
+        System.out.println("making local tunnel");
         Socket local = getLocalTunnel();
+        System.out.println("got local tunnel");
+
         if (local == null) {
             System.out.println("failed to setup local tunnel, aborting");
             throw new SocketException("failed to setup local tunnel");
         }
 
         try {
-            Thread out = new Forwarder(local.getOutputStream(), remote.getInputStream());
-            Thread in  = new Forwarder(remote.getOutputStream(), local.getInputStream());
+            Thread out = new Forwarder(local.getOutputStream(), this.remote.getInputStream());
+            Thread in  = new Forwarder(this.remote.getOutputStream(), local.getInputStream());
 
             out.start();
             in.start();
@@ -110,77 +113,6 @@ public class Tunnel {
                 System.out.println("failed to setup local tunnel: " + e.getMessage());
                 return null;
             }
-        }
-    }
-
-    public void startAsClient() {
-        System.out.printf("Starting hole punching: [:%d] --- [%s:%d]\n",
-                sourcePort, target.getAddress(), target.getPort());
-
-        Socket server = new Socket();
-
-        try {
-            server.setReuseAddress(true);
-            server.bind(new InetSocketAddress(sourcePort));
-        } catch (IOException e) {
-            System.out.println("failed to bind to socket, might be in use");
-        }
-
-        for (int i = 0; i < 20; i++) {
-            try {
-                server.connect(target, 2000);
-                System.out.println("connection established successfully! Connect to localhost now");
-
-                ServerSocket local = new ServerSocket();
-                local.setReuseAddress(true);
-                local.bind(new InetSocketAddress(sourcePort + 1));
-                Socket client = local.accept();
-
-                Thread c2s = new Forwarder(client.getOutputStream(), server.getInputStream());
-                Thread s2c = new Forwarder(server.getOutputStream(), client.getInputStream());
-
-                c2s.start();
-                s2c.start();
-
-                local.close();
-
-            } catch (IOException e) {
-                System.out.println("failed connection attempt " + (i + 1) + "/20");
-                try { Thread.sleep(2000); } catch (InterruptedException ignored) {}
-            }
-        }
-
-        try {
-            server.close();
-        } catch (IOException e) {
-            System.out.println("failed to close server socket. oh well");
-        }
-    }
-
-    public void startAsServer() {
-        System.out.printf("Starting hole punching: [:%d] --- [%s:%d]\n",
-                sourcePort, target.getAddress(), target.getPort());
-
-        try (Socket s = new Socket()) {
-            s.setReuseAddress(true);
-            s.bind(new InetSocketAddress(sourcePort));
-            s.connect(target, 500);
-        } catch (IOException e) {
-            System.out.println("punch packet sent (failed successfully), err: " + e.getMessage());
-        }
-
-        try (ServerSocket sock = new ServerSocket(sourcePort)) {
-            Socket client = sock.accept();
-            Socket server = new Socket("localhost", 25565);
-
-            Thread c2s = new Forwarder(client.getOutputStream(), server.getInputStream());
-            Thread s2c = new Forwarder(server.getOutputStream(), client.getInputStream());
-
-            c2s.start();
-            s2c.start();
-
-        } catch (IOException e) {
-
         }
     }
 
