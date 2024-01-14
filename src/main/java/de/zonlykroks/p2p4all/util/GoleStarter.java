088c9d0555b93af7091801bfe9cec57e6e0d3c55
zOnlyKroks <finnrades@gmail.com>
2024-01-14 13:11:28 +0100

    Be smart for once, twice

--- src/main/java/de/zonlykroks/p2p4all/util/GoleStarter.java
+++ src/main/java/de/zonlykroks/p2p4all/util/GoleStarter.java
@@ -73,7 +73,7 @@ public class GoleStarter {
                     return;
                 }
 
-                if(!future.isCancelled()) {
+                if(parent.ipToStateMap.get(targetIp) != ConnectionProgress.FAILED) {
                     if (!areWeTheServer) {
                         System.out.println("Connection established!\n\nWaiting for you to join @ 127.0.0.1:" + gamePort);
                         P2P4AllClient.SERVER_CONNECT_ADDRESS = "127.0.0.1:" + gamePort;
