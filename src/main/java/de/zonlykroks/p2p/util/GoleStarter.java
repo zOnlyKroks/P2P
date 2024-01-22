b7a0b67c39fd5c943ac97a9a681525f857fc6e83
zOnlyKroks <64548817+zOnlyKroks@users.noreply.github.com>
2024-01-22 08:46:50 +0100

    Bit of formatting

--- src/main/java/de/zonlykroks/p2p/util/GoleStarter.java
+++ src/main/java/de/zonlykroks/p2p/util/GoleStarter.java
@@ -29,15 +29,9 @@ public class GoleStarter {
         GoleAPIEvents.IP_STATE_CHANGE.invoker().ipStateChange(targetIp, null , ConnectionProgress.PENDING);
         new Thread(() -> {
             try {
-                int gamePort = areWeTheServer ? P2PYACLConfig.get().localServerPort : P2PYACLConfig.get().localClientGamePort;
+                if(!validateIPAddress()) return;
 
-                try {
-                    InetAddress.getByName(targetIp);
-                } catch (Exception ex) {
-                    System.out.println("Couldn't parse IP address \"" + targetIp + "\"");
-                    P2PClient.ipToStateMap.put(targetIp,ConnectionProgress.FAILED);
-                    return;
-                }
+                int gamePort = areWeTheServer ? P2PYACLConfig.get().localServerPort : P2PYACLConfig.get().localClientGamePort;
 
                 int port = Integer.parseInt(password);
                 int port1 = areWeTheServer ? port + 1 : port;
@@ -48,62 +42,90 @@ public class GoleStarter {
 
                 P2PClient.currentlyRunningTunnels.put(targetIp, process);
 
-                long wait = System.currentTimeMillis() + (P2PYACLConfig.get().connectTimeoutInSeconds * 1000L);
-                while (!future.isDone() && System.currentTimeMillis() <= wait) {
-                    //Do nothing
-                }
+                if(!handleWait(future)) return;
 
-                if (!future.isDone()) {
-                    future.cancel(true);
-                    System.out.println("Failed to connect after 2 minutes and 30 seconds");
-                    P2PClient.ipToStateMap.put(targetIp,ConnectionProgress.FAILED);
-                    GoleAPIEvents.IP_STATE_CHANGE.invoker().ipStateChange(targetIp, ConnectionProgress.PENDING , ConnectionProgress.FAILED);
-                    return;
-                }
+                handleFinish(gamePort, port1, port2);
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
 
-                if(P2PClient.ipToStateMap.get(targetIp) == ConnectionProgress.SUCCESS) {
-                    if (!areWeTheServer) {
-                        System.out.println("Connection established!\n\nWaiting for you to join @ 127.0.0.1:" + gamePort);
-                        P2PClient.SERVER_CONNECT_ADDRESS = "127.0.0.1:" + gamePort;
+    private boolean handleWait(CompletableFuture<Void> future) {
+        long wait = System.currentTimeMillis() + (P2PYACLConfig.get().connectTimeoutInSeconds * 1000L);
+        while (!future.isDone() && System.currentTimeMillis() <= wait) {
+            //Do nothing
+        }
 
-                        new Thread(() -> {
-                            try {
-                                Thread.sleep(6000);
-                                System.out.println("A");
+        if (!future.isDone()) {
+            future.cancel(true);
+            System.out.println("Failed to connect after 2 minutes and 30 seconds");
+            P2PClient.ipToStateMap.put(targetIp,ConnectionProgress.FAILED);
+            GoleAPIEvents.IP_STATE_CHANGE.invoker().ipStateChange(targetIp, ConnectionProgress.PENDING , ConnectionProgress.FAILED);
+            return false;
+        }
+        return true;
+    }
 
-                                Socket socket = new Socket();
-                                socket.connect(new InetSocketAddress(InetAddress.getByName(targetIp), port2));
-                                System.out.println(socket.getInetAddress() + ":" + port2);
+    private void handleFinish(int gamePort, int port1, int port2) {
+        if(P2PClient.ipToStateMap.get(targetIp) == ConnectionProgress.SUCCESS) {
+            if (!areWeTheServer) {
+                System.out.println("Connection established!\n\nWaiting for you to join @ 127.0.0.1:" + gamePort);
+                P2PClient.SERVER_CONNECT_ADDRESS = "127.0.0.1:" + gamePort;
 
-                                PrintWriter writer = new PrintWriter(socket.getOutputStream());
-                                writer.println("TEST!");
-                                System.out.println("B");
-                                System.exit(-1);
-                            }catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        }).start();
+                handleClient(port2);
+            } else {
+                System.out.println("Connection established!\nWaiting for the player to join");
 
-                    } else {
-                        System.out.println("Connection established!\nWaiting for the player to join");
+                handleServer(port1);
+            }
+        }
+    }
 
-                        new Thread(() -> {
-                            try {
-                                ServerSocket serverSocket = new ServerSocket(port1);
-                                System.out.println(serverSocket.getInetAddress() + ":" + port1);
-                                Socket sck = serverSocket.accept();
-                                BufferedReader reader = new BufferedReader(new InputStreamReader(sck.getInputStream()));
+    private boolean validateIPAddress() {
+        try {
+            InetAddress.getByName(targetIp);
+            return true;
+        } catch (Exception ex) {
+            System.out.println("Couldn't parse IP address \"" + targetIp + "\"");
+            P2PClient.ipToStateMap.put(targetIp,ConnectionProgress.FAILED);
+            return false;
+        }
+    }
 
-                                System.out.println("A");
-                                System.out.println(reader.readLine());
-                                System.out.println("B");
-                                System.exit(-1);
-                            }catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        }).start();
-                    }
-                }
+    private void handleClient(int port2) {
+        new Thread(() -> {
+            try {
+                Thread.sleep(6000);
+                System.out.println("A");
+
+                Socket socket = new Socket();
+                socket.connect(new InetSocketAddress(InetAddress.getByName(targetIp), port2));
+                System.out.println(socket.getInetAddress() + ":" + port2);
+
+                PrintWriter writer = new PrintWriter(socket.getOutputStream());
+                writer.println("TEST!");
+                System.out.println("B");
+                System.exit(-1);
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+    }
+
+    private void handleServer(int port1) {
+        new Thread(() -> {
+            try {
+                ServerSocket serverSocket = new ServerSocket(port1);
+                System.out.println(serverSocket.getInetAddress() + ":" + port1);
+                Socket sck = serverSocket.accept();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(sck.getInputStream()));
+
+                System.out.println("A");
+                System.out.println(reader.readLine());
+                System.out.println("B");
+                System.exit(-1);
             }catch (Exception e) {
                 e.printStackTrace();
             }
