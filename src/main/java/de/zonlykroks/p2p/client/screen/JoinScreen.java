8f0e24a0cbde7cfd921fe7d41b44c01ba54dccad
zOnlyKroks <finnrades@gmail.com>
2024-02-24 15:37:33 +0100

    Be smart

--- src/main/java/de/zonlykroks/p2p/client/screen/JoinScreen.java
+++ src/main/java/de/zonlykroks/p2p/client/screen/JoinScreen.java
@@ -52,7 +52,7 @@ public class JoinScreen extends Screen {
             GoleStarter goleStarter = new GoleStarter(ip,portWidget.getText(),false);
             goleStarter.start();
 
-            ServerInfo info = new ServerInfo("P2P", "127.0.0.1:39332", false);
+            ServerInfo info = new ServerInfo("P2P", "127.0.0.1:" + P2PYACLConfig.get().localClientGamePort, false);
 
             MinecraftClient.getInstance().setScreen(new SingleConnectionStateScreen(this, () -> {
                 MinecraftClient.getInstance().setScreen(new DirectConnectScreen(new MultiplayerScreen(this), b -> {
